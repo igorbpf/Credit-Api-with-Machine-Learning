340f2c6d007a191fa6e271f228fc4586821e983b
Igor Santos <igorbpf@gmail.com>
2017-01-11 19:50:01 -0200

    Initial commit

---
+++ mtbank.py
@@ -0,0 +1,119 @@
+from flask import Flask
+from flask_restplus import Api, fields, Resource
+from sklearn.externals import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+api = Api(app,
+        version='1.0',
+        title='Credit Prediction API',
+        description='Machine Learning for Credit Prediction')
+
+mt = api.namespace('approve_credit',
+    description='Credit operations')
+
+parser = api.parser()
+parser.add_argument('RevolvingUtilizationOfUnsecuredLines',
+                    type=float,
+                    required=True,
+                    help="Total balance on credit cards and pesonal lines of credit except real estate and no installment debt like car loans divided by the sum of credit limits",
+                    location='form')
+
+parser.add_argument('Age',
+                    type=int,
+                    required=True,
+                    help='Age of borrower in years',
+                    location='form')
+
+parser.add_argument('NumberOfTime30-59DaysPastDueNotWorse',
+                    type=int,
+                    required=True,
+                    help='Number of times borrower has been 30-59 days past due but no worse in the last 2 years.',
+                    location='form')
+
+parser.add_argument('DebtRatio',
+                    type=float,
+                    required=True,
+                    help='Monthly debt payments, alimony,living costs divided by monthy gross income',
+                    location='form')
+
+parser.add_argument('MonthlyIncome',
+                    type=float,
+                    required=True,
+                    help='Monthly income',
+                    location='form')
+
+parser.add_argument('NumberOfOpenCreditLinesAndLoans',
+                    type=int,
+                    required=True,
+                    help='Number of Open loans (installment like car loan or mortgage) and Lines of credit (e.g. credit cards)',
+                    location='form')
+
+parser.add_argument('NumberOfTimes90DaysLate',
+                    type=int,
+                    required=True,
+                    help='Number of times borrower has been 90 days or more past due.',
+                    location='form')
+
+parser.add_argument('NumberRealEstateLoansOrLines',
+                    type=int,
+                    required=True,
+                    help='Number of mortgage and real estate loans including home equity lines of credit',
+                    location='form')
+
+parser.add_argument('NumberOfTime60-89DaysPastDueNotWorse',
+                    type=int,
+                    required=True,
+                    help='Number of times borrower has been 60-89 days past due but no worse in the last 2 years.',
+                    location='form')
+
+
+parser.add_argument('NumberOfDependents',
+                    type=int,
+                    required=True,
+                    help='Number of dependents in family excluding themselves (spouse, children etc.)',
+                    location='form')
+
+resource_fields = api.model('Resource', {'result': fields.String,})
+
+@mt.route('/')
+class MtBank(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def post(self):
+        args = parser.parse_args()
+        result = self.get_result(args)
+        return result, 201
+
+    def get_result(self, args):
+        debtRatio = args["DebtRatio"]
+        monthlyIncome = args["MonthlyIncome"]
+        dependents = args["NumberOfDependents"]
+        openCreditLinesAndLoans = args["NumberOfOpenCreditLinesAndLoans"]
+        pastDue30Days = args["NumberOfTime30-59DaysPastDueNotWorse"]
+        pastDue60Days = args["NumberOfTime60-89DaysPastDueNotWorse"]
+        pastDue90Days = args["NumberOfTimes90DaysLate"]
+        realEstateLoansOrLines = args["NumberRealEstateLoansOrLines"]
+        unsecuredLines = args["RevolvingUtilizationOfUnsecuredLines"]
+        age = args["Age"]
+
+
+        X = np.array([[unsecuredLines, age, pastDue30Days,
+                    debtRatio, monthlyIncome, openCreditLinesAndLoans,
+                    pastDue90Days, realEstateLoansOrLines,
+                    pastDue60Days, dependents]])
+
+        clf = joblib.load('classifier.pkl')
+        result = clf.predict(X)
+
+        if result[0] == 1.0:
+            result = "Denied"
+        else:
+            result = "Approved"
+
+        return {"result": result}
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ train_model.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import auc, confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.externals import joblib
+
+data = pd.read_csv("data/cs-training.csv")
+
+# Data cleaning
+data.dropna(inplace=True)
+data.drop('Unnamed: 0', axis=1, inplace=True)
+y = data['SeriousDlqin2yrs']
+data.drop('SeriousDlqin2yrs', axis=1, inplace=True)
+X = data
+
+# Data Normalization
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(X)
+
+# Data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=7)
+
+# Creating the classifier
+clf = LogisticRegression(class_weight="balanced")
+
+# Training
+clf.fit(X_train, y_train)
+
+# Assessing the model
+
+print "The confusion matrix is:"
+
+y_pred = clf.predict(X_test)
+
+print confusion_matrix(y_test, y_pred)
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred, pos_label=1)
+
+print "The Area under the curve (AUC) is:"
+
+print auc(fpr, tpr)
+
+# Saving the classifier
+
+joblib.dump(clf, 'meetup_classifier.pkl')
+
+print "The classifier was saved with success!!!!"
+
+
+
+
+
+
+
+
+
+
+
